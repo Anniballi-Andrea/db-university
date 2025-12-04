067f92add94d24caa9aa66c805393cb708baae96
Anniballi-Andrea <andreanniballi@gmail.com>
2025-12-04 16:25:49 +0100

    ex queries select 3 done

--- ex_queries.sql
+++ ex_queries.sql
@@ -1,10 +1,15 @@
 #Selezionare tutti gli studenti nati nel 1990 (160)
 
-SELECT *
+/*SELECT *
 FROM `students`
-WHERE YEAR(`date_of_birth`) = 1990;
+WHERE YEAR(`date_of_birth`) = 1990;*/
 
 #Selezionare tutti i corsi che valgono più di 10 crediti (479)
-SELECT*
+/*SELECT*
 FROM `courses`
-WHERE `cfu` > 10
+WHERE `cfu` > 10*/
+
+#Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT *
+FROM `students`
+WHERE YEAR(`date_of_birth`) < 1995;
