18e9f7ffbeb9f1eee0727539109684e102f0732e
Anniballi-Andrea <andreanniballi@gmail.com>
2025-12-04 16:46:29 +0100

    ex queries select 7 done

--- ex_queries.sql
+++ ex_queries.sql
@@ -21,7 +21,7 @@ FROM `courses`
 WHERE `year` =1
 AND `period` = "I semestre"*/
 
-#Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+#Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)'
 
 /*SELECT *
 FROM `exams`
@@ -30,6 +30,10 @@ AND `date` = "2020-06-20"*/
 
 #Selezionare tutti i corsi di laurea magistrale (38)
 
-SELECT *
+/*SELECT *
 FROM `degrees`
-WHERE `name` LIKE "Corso di Laurea Magistrale%"
+WHERE `name` LIKE "Corso di Laurea Magistrale%"*/
+
+#Da quanti dipartimenti è composta l'università? (12)
+SELECT *
+FROM `departments`
