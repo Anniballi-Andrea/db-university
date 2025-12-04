a683412435d37b182559f43dec7c5fe16327efc4
Anniballi-Andrea <andreanniballi@gmail.com>
2025-12-04 17:28:34 +0100

    ex queries group 2 done

--- ex_queries.sql
+++ ex_queries.sql
@@ -50,6 +50,11 @@ WHERE `phone` IS null*/
 
 #Contare quanti iscritti ci sono stati ogni anno
 
-SELECT  COUNT(id),YEAR(enrolment_date)
+/*SELECT  COUNT(id),YEAR(`enrolment_date`)
 FROM `students`
-GROUP BY YEAR(enrolment_date)
+GROUP BY YEAR(`enrolment_date`)*/
+
+#Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT COUNT(id), `office_address`
+FROM `teachers`
+GROUP BY `office_address`
