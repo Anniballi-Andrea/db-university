14947d8b69452eb7de97372a65a982bb2be9c44d
Anniballi-Andrea <andreanniballi@gmail.com>
2025-12-04 17:36:51 +0100

    ex query group 3 done

--- ex_queries.sql
+++ ex_queries.sql
@@ -51,10 +51,17 @@ WHERE `phone` IS null*/
 #Contare quanti iscritti ci sono stati ogni anno
 
 /*SELECT  COUNT(id),YEAR(`enrolment_date`)
+
 FROM `students`
 GROUP BY YEAR(`enrolment_date`)*/
 
 #Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-SELECT COUNT(id), `office_address`
+
+/*SELECT COUNT(id), `office_address`
 FROM `teachers`
-GROUP BY `office_address`
+GROUP BY `office_address`*/
+
+#Calcolare la media dei voti di ogni appello d'esame
+
+SELECT AVG(`vote`)
+FROM `exam_student`
