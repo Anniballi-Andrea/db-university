e0293ecacae9cff3e911a5e430e80f4319852803
Anniballi-Andrea <andreanniballi@gmail.com>
2025-12-05 15:29:19 +0100

    ex day twoo add to yesterday file

--- ex_queries.sql
+++ ex_queries.sql
@@ -1,46 +1,46 @@
-#Selezionare tutti gli studenti nati nel 1990 (160)
+1#Selezionare tutti gli studenti nati nel 1990 (160)
 
 /SELECT *
 FROM `students`
 WHERE YEAR(`date_of_birth`) = 1990;
 
-#Selezionare tutti i corsi che valgono più di 10 crediti (479)
+2#Selezionare tutti i corsi che valgono più di 10 crediti (479)
 SELECT*
 FROM `courses`
 WHERE `cfu` > 10
 
-#Selezionare tutti gli studenti che hanno più di 30 anni
+3#Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT *
 FROM `students`
 WHERE YEAR(`date_of_birth`) < 1995;
 
-#Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+4#Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
 SELECT *
 FROM `courses`
 WHERE `year` =1
 AND `period` = "I semestre"
 
-#Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)'
+5#Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)'
 
 SELECT *
 FROM `exams`
 WHERE hour(`hour`) > 13
 AND `date` = "2020-06-20"
 
-#Selezionare tutti i corsi di laurea magistrale (38)
+6#Selezionare tutti i corsi di laurea magistrale (38)
 
 SELECT *
 FROM `degrees`
 WHERE `name` LIKE "Corso di Laurea Magistrale%"
 
-#Da quanti dipartimenti è composta l'università? (12)'
+7#Da quanti dipartimenti è composta l'università? (12)'
 
 SELECT COUNT(id)
 FROM `departments`
 
 
-#Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+8#Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
 SELECT COUNT(id)
 FROM `teachers`
@@ -71,3 +71,14 @@ FROM `exam_student`
 SELECT count(`id`), `degree_id`
 FROM `courses`
 group by `degree_id`
+
+-------------Day_Twoo--------------
+
+#1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT `students`.`name`, `students`.`surname`, `degrees`.`name` AS `degree_name`
+FROM `students`
+JOIN `degrees`
+WHERE `degrees`.`name`= "Corso di Laurea in Economia"
+
+
+
